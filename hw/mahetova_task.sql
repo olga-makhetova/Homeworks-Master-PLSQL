1aa8f416a736604e0e2929b08aaf91d0877e4224
olga.makhetova <7261602+olga-makhetova@users.noreply.github.com>
2025-05-06 14:20:34 +0400

    ДЗ 10. Работа с коллекциями (#12)
    * ДЗ 8. Object
    
    * ДЗ 9. Коллекции
    
    * ДЗ 10. Работа с коллекциями

--- hw/mahetova_task.sql
+++ hw/mahetova_task.sql
@@ -9,16 +9,30 @@ declare
   v_payment_id payment.payment_id%type := 0;
   v_date date := sysdate;
   v_msg varchar2(250 char) := 'Платеж создан. Статус: ' || c_status_created;
-	v_payment t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_1'),
-																														 t_payment_detail(2, 'IP_1'),
-																														 t_payment_detail(3, 'NOTE_1'),
-																														 t_payment_detail(4, 'IS_CHECKED_FRAUD_1')
-																														);
+	v_payment_data t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_1'),
+																																	t_payment_detail(2, 'IP_1'),
+																																	t_payment_detail(3, 'NOTE_1'),
+																																	t_payment_detail(4, 'IS_CHECKED_FRAUD_1')
+																																	);
 begin
   dbms_output.put_line('Текущая дата: ' || to_char(v_date, 'dd mon year'));
   dbms_output.put_line(v_msg);
 
   dbms_output.put_line('v_payment_id = ' || v_payment_id);
+  
+  if v_payment_data is not empty then
+    for i in v_payment_data.first..v_payment_data.last loop
+    	if v_payment_data(i).field_type is null then
+    	  dbms_output.put_line('ID поля не может быть пустым');
+    	end if;
+    	if v_payment_data(i).field_value is null then
+    	  dbms_output.put_line('Значение в поле не может быть пустым');
+    	end if;
+  	  dbms_output.put_line('field_type = ' || v_payment_data(i).field_type || ', field_value = ' || v_payment_data(i).field_value );
+    end loop;
+  else
+    dbms_output.put_line('Коллекция не содержит данных');
+  end if;
 end;
 /
 
@@ -88,11 +102,11 @@ declare
   v_payment_id payment.payment_id%type := 0;
   v_date date := sysdate;
   v_msg varchar2(250 char) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
-	v_payment t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_2'),
-																														 t_payment_detail(2, 'IP_2'),
-																														 t_payment_detail(3, 'NOTE_2'),
-																														 t_payment_detail(4, 'IS_CHECKED_FRAUD_2')
-																														);
+	v_payment_data t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_2'),
+																																  t_payment_detail(2, 'IP_2'),
+																																  t_payment_detail(3, 'NOTE_2'),
+																																  t_payment_detail(4, 'IS_CHECKED_FRAUD_2')
+																																 );
 begin
   dbms_output.put_line('Текущая дата: ' || to_char(v_date, 'dd.mm.yyyy hh:mi AM'));
   dbms_output.put_line(v_msg);
@@ -100,6 +114,20 @@ begin
   dbms_output.put_line('v_payment_id = ' || v_payment_id);
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
+  end if;
+  
+  if v_payment_data is not empty then
+    for i in v_payment_data.first..v_payment_data.last loop
+    	if v_payment_data(i).field_type is null then
+    	  dbms_output.put_line('ID поля не может быть пустым');
+    	end if;
+    	if v_payment_data(i).field_value is null then
+    	  dbms_output.put_line('Значение в поле не может быть пустым');
+    	end if;
+  	  dbms_output.put_line('field_type = ' || v_payment_data(i).field_type || ', field_value = ' || v_payment_data(i).field_value );
+    end loop;
+  else
+    dbms_output.put_line('Коллекция не содержит данных');
   end if;
 end;
 /
@@ -118,5 +146,10 @@ begin
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
   end if;
+  
+  if v_payment_delete_ids is empty then
+    dbms_output.put_line('Коллекция не содержит данных');
+  end if;
+  dbms_output.put_line('Количество полей для удаления = ' || v_payment_delete_ids.count());
 end;
 /
