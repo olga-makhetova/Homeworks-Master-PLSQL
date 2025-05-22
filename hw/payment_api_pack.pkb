b5c6d93309c1d3697648ab3c7dff2659fdf7bb47
olga.makhetova <olga.makhetova@gmail.com>
2025-05-22 13:11:52 +0400

    ДЗ 14. Исключения

---
+++ hw/payment_api_pack.pkb
@@ -0,0 +1,126 @@
+create or replace package body payment_api_pack
+is
+
+  -- Создание платежа
+  function create_payment(p_payment_data   t_payment_detail_array,
+                          p_summa          payment.summa%type,
+                          p_currency_id    payment.currency_id%type,
+                          p_from_client_id payment.from_client_id%type,
+                          p_to_client_id   payment.to_client_id%type,
+                          p_create_dtime   payment.create_dtime%type
+                          ) return payment.payment_id%type 
+  is
+    v_payment_id payment.payment_id%type;
+    v_msg varchar2(250 char) := 'Платеж создан. Статус: ' || c_status_created;
+  begin
+    dbms_output.put_line('Дата платежа: ' || to_char(p_create_dtime, 'dd mon year'));
+    
+    if p_payment_data is not empty then
+      for i in p_payment_data.first..p_payment_data.last loop
+        if p_payment_data(i).field_type is null then
+          raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_field_id);
+        end if;
+        if p_payment_data(i).field_value is null then
+          raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_field_value);
+        end if;
+        dbms_output.put_line('field_type = ' || p_payment_data(i).field_type || ', field_value = ' || p_payment_data(i).field_value );
+      end loop;
+    else
+      raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_collection);
+    end if;
+    
+    -- добавление платежа
+    insert into payment(payment_id, create_dtime, summa, currency_id, from_client_id, to_client_id)
+      values(payment_seq.nextval, p_create_dtime, p_summa, p_currency_id, p_from_client_id, p_to_client_id)
+      returning payment_id into v_payment_id;
+    
+    -- добавление деталей платежа из коллекции
+    insert into payment_detail(payment_id, field_id, field_value)
+      select v_payment_id, value(t).field_type, value(t).field_value from table(p_payment_data) t; 
+    
+    dbms_output.put_line(v_msg);
+    dbms_output.put_line('v_payment_id = ' || v_payment_id);
+    
+    return v_payment_id;
+  end create_payment;
+
+  -- Сброс платежа в "ошибочный статус" с указанием причины
+  procedure fail_payment(p_payment_id payment.payment_id%type,
+                         p_reason     payment.status_change_reason%type
+                         )
+  is
+    v_date date := sysdate;
+    v_msg varchar2(250 char) := 'Сброс платежа в "ошибочный статус" с указанием причины. Статус: ' || c_status_error || '. Причина: ' || p_reason;
+  begin
+    dbms_output.put_line('Текущая дата: ' || to_char(v_date, 'dd FMmonth (dy)'));
+    dbms_output.put_line(v_msg);
+
+    if p_payment_id is null then
+      raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_object_id);
+    end if;
+    dbms_output.put_line('p_payment_id = ' || p_payment_id);
+
+    if p_reason is null then
+      raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_reason);
+    end if;
+      
+    update payment
+       set status = c_status_error,
+           status_change_reason = p_reason
+     where payment_id = p_payment_id 
+       and status = c_status_created; 
+    
+    dbms_output.put_line(sql%rowcount);
+  end fail_payment;
+
+
+  -- Отмена платежа с указанием причины
+  procedure cancel_payment(p_payment_id payment.payment_id%type,
+                           p_reason     payment.status_change_reason%type
+                           )
+  is
+    v_date date := sysdate;
+    v_msg varchar2(250 char) := 'Отмена платежа с указанием причины. Статус: ' || c_status_canceled || '. Причина: ' || p_reason;
+  begin
+    dbms_output.put_line('Текущая дата: ' || to_char(v_date, 'dd.FMRM.yyyy'));
+    dbms_output.put_line(v_msg);
+
+    if p_payment_id is null then
+      raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_object_id);
+    end if;
+    dbms_output.put_line('p_payment_id = ' || p_payment_id);
+
+    if p_reason is null then
+      raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_reason);
+    end if;
+      
+    update payment
+       set status = c_status_canceled,
+           status_change_reason = p_reason
+     where payment_id = p_payment_id 
+       and status = c_status_created; 
+  end cancel_payment;
+
+  -- Успешное завершение платежа
+  procedure successful_finish_payment(p_payment_id payment.payment_id%type)
+  is
+    v_date date := sysdate;
+    v_msg varchar2(250 char) := 'Успешное завершение платежа. Статус: ' || c_status_finished;
+  begin
+    dbms_output.put_line('Текущая дата: ' || to_char(v_date, 'dd.mm.yy hh24:mi:ss'));
+    dbms_output.put_line(v_msg);
+
+    if p_payment_id is null then
+      raise_application_error(c_error_code_invalid_input_parameter, c_err_msg_empty_object_id);
+    end if;
+    dbms_output.put_line('p_payment_id = ' || p_payment_id);
+      
+    update payment
+       set status = c_status_finished,
+            status_change_reason = ''
+     where payment_id = p_payment_id 
+       and status = c_status_created; 
+  end successful_finish_payment;
+    
+end payment_api_pack;
+/
--- hw/unit_tests.sql
+++ hw/unit_tests.sql
@@ -3,8 +3,6 @@
   описание скрипта: unit-тесты для API для сущностей "Платеж" и "Детали платежа"
 */
 
-select * from payment;
-
 -- Создание платежа
 declare
   v_payment_data t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_1'),
@@ -18,86 +16,171 @@ declare
   v_to_client_id   payment.to_client_id%type := 2;
   v_payment_id     payment.payment_id%type;
 begin
-  v_payment_id := create_payment(v_payment_data, 
-                                 v_summa, 
-                                 v_currency_id,
-                                 v_from_client_id, 
-                                 v_to_client_id
-                                 );
-  commit;
+  v_payment_id := payment_api_pack.create_payment(v_payment_data, 
+                                                  v_summa, 
+                                                  v_currency_id,
+                                                  v_from_client_id, 
+                                                  v_to_client_id,
+                                                  systimestamp
+                                                  );
 end;
 /
-
-select * from payment;
-select * from payment_detail;
 
 -- Сброс платежа в "ошибочный статус" с указанием причины
 declare
-  v_payment_id payment.payment_id%type := 1;
+  v_payment_id payment.payment_id%type := 21;
   v_reason payment.status_change_reason%type := 'недостаточно средств';
 begin
-  fail_payment(p_payment_id => v_payment_id,
-               p_reason     => v_reason
-               );
-  commit;
+  payment_api_pack.fail_payment(p_payment_id => v_payment_id,
+                                p_reason     => v_reason
+                                );
 end;
 /
-
-select * from payment;
 
 -- Отмена платежа с указанием причины
 declare
-  v_payment_id payment.payment_id%type := 2;
-  v_reason payment.status_change_reason%type := 'ошибка пользовател¤';
+  v_payment_id payment.payment_id%type := 22;
+  v_reason payment.status_change_reason%type := 'ошибка пользовател§';
 begin
-  cancel_payment(p_payment_id => v_payment_id,
-                 p_reason     => v_reason
-                 );
-  commit;
+  payment_api_pack.cancel_payment(p_payment_id => v_payment_id,
+                                  p_reason     => v_reason
+                                  );
 end;
 /
-
-select * from payment;
 
 -- Успешное завершение платежа
 declare
-  v_payment_id payment.payment_id%type := 3;
+  v_payment_id payment.payment_id%type := 23;
 begin
-  successful_finish_payment(p_payment_id => v_payment_id);
-  commit;
+  payment_api_pack.successful_finish_payment(p_payment_id => v_payment_id);
 end;
 /
 
-select * from payment;
-select * from payment_detail;
-
 -- Данные платежа добавлены или обновлены
 declare
-  v_payment_id payment.payment_id%type := 1;
+  v_payment_id payment.payment_id%type := 21;
   v_payment_data t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_2'),
                                                                   t_payment_detail(2, 'IP_2'),
                                                                   t_payment_detail(3, 'NOTE_2'),
                                                                   t_payment_detail(4, 'IS_CHECKED_FRAUD_2')
                                                                  );
 begin
-  insert_or_update_payment_detail(p_payment_id   => v_payment_id,
-                                  p_payment_data => v_payment_data
-                                 ); 
-  commit;
+  payment_detail_api_pack.insert_or_update_payment_detail(p_payment_id   => v_payment_id,
+                                                          p_payment_data => v_payment_data
+                                                          ); 
 end;
 /
-
-select * from payment_detail;
 
 -- Детали платежа удалены
 declare
-  v_payment_id payment.payment_id%type := 1;
+  v_payment_id payment.payment_id%type := 21;
   v_payment_delete_ids t_number_array := t_number_array(1, 2, 3);
 begin
-  delete_payment_detail(p_payment_id         => v_payment_id,
-                        p_payment_delete_ids => v_payment_delete_ids
-                       );
-  commit;
+  payment_detail_api_pack.delete_payment_detail(p_payment_id         => v_payment_id,
+                                                p_payment_delete_ids => v_payment_delete_ids
+                                                );
 end;
 /
-select * from payment_detail;
+
+/****************************************
+**       Негативные unit-тесты         **
+****************************************/
+-- Создание платежа
+declare
+  v_payment_data t_payment_detail_array;                                        
+  v_summa          payment.summa%type := 1000;
+  v_currency_id    payment.currency_id%type := 840;
+  v_from_client_id payment.from_client_id%type := 1;
+  v_to_client_id   payment.to_client_id%type := 2;
+  v_payment_id     payment.payment_id%type;
+begin
+  v_payment_id := payment_api_pack.create_payment(v_payment_data, 
+                                                  v_summa, 
+                                                  v_currency_id,
+                                                  v_from_client_id, 
+                                                  v_to_client_id,
+                                                  systimestamp
+                                                  );
+  raise_application_error(-20999, 'Unit-тесты или API выполнены неверно!');
+exception
+  when payment_api_pack.e_invalid_input_parameter then
+    dbms_output.put_line('Создание платежа. Исключение возбуждено успешно. Ошибка: ' || sqlerrm);
+end;
+/
+
+
+-- Сброс платежа в "ошибочный статус" с указанием причины
+declare
+  v_payment_id payment.payment_id%type;
+  v_reason payment.status_change_reason%type := 'недостаточно средств';
+begin
+  payment_api_pack.fail_payment(p_payment_id => v_payment_id,
+                                p_reason     => v_reason
+                                );
+  raise_application_error(-20999, 'Unit-тесты или API выполнены неверно!');
+exception
+  when payment_api_pack.e_invalid_input_parameter then
+    dbms_output.put_line('Сброс платежа в "ошибочный статус" с указанием причины. Исключение возбуждено успешно. Ошибка: ' || sqlerrm);
+end;
+/
+
+-- Отмена платежа с указанием причины
+declare
+  v_payment_id payment.payment_id%type := 22;
+  v_reason payment.status_change_reason%type;
+begin
+  payment_api_pack.cancel_payment(p_payment_id => v_payment_id,
+                                  p_reason     => v_reason
+                                  );
+  raise_application_error(-20999, 'Unit-тесты или API выполнены неверно!');
+exception
+  when payment_api_pack.e_invalid_input_parameter then
+    dbms_output.put_line('Отмена платежа с указанием причины. Исключение возбуждено успешно. Ошибка: ' || sqlerrm);
+end;
+/
+
+-- Успешное завершение платежа
+declare
+  v_payment_id payment.payment_id%type;
+begin
+  payment_api_pack.successful_finish_payment(p_payment_id => v_payment_id);
+  raise_application_error(-20999, 'Unit-тесты или API выполнены неверно!');
+exception
+  when payment_api_pack.e_invalid_input_parameter then
+    dbms_output.put_line('Успешное завершение платежа. Исключение возбуждено успешно. Ошибка: ' || sqlerrm);
+end;
+/
+
+-- Данные платежа добавлены или обновлены
+declare
+  v_payment_id payment.payment_id%type;
+  v_payment_data t_payment_detail_array := t_payment_detail_array(t_payment_detail(1, 'CLIENT_SOFTWARE_2'),
+                                                                  t_payment_detail(2, 'IP_2'),
+                                                                  t_payment_detail(3, 'NOTE_2'),
+                                                                  t_payment_detail(4, 'IS_CHECKED_FRAUD_2')
+                                                                 );
+begin
+  payment_detail_api_pack.insert_or_update_payment_detail(p_payment_id   => v_payment_id,
+                                                          p_payment_data => v_payment_data
+                                                          ); 
+  raise_application_error(-20999, 'Unit-тесты или API выполнены неверно!');
+exception
+  when payment_api_pack.e_invalid_input_parameter then
+    dbms_output.put_line('Данные платежа добавлены или обновлены. Исключение возбуждено успешно. Ошибка: ' || sqlerrm);
+end;
+/
+
+-- Детали платежа удалены
+declare
+  v_payment_id payment.payment_id%type := 21;
+  v_payment_delete_ids t_number_array := t_number_array();
+begin
+  payment_detail_api_pack.delete_payment_detail(p_payment_id         => v_payment_id,
+                                                p_payment_delete_ids => v_payment_delete_ids
+                                                );
+  raise_application_error(-20999, 'Unit-тесты или API выполнены неверно!');
+exception
+  when payment_api_pack.e_invalid_input_parameter then
+    dbms_output.put_line('Детали платежа удалены. Исключение возбуждено успешно. Ошибка: ' || sqlerrm);
+end;
+/
